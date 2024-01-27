f04c67f033f113ccb111a9807a9b658eae05f353
thaihd <thaihd@athena.studio>
2024-01-27 13:33:55 +0700

    connect local db

--- ThaiHD Folder/Camera.py
+++ ThaiHD Folder/Camera.py
@@ -5,12 +5,32 @@ Created on Tue Jan 23 19:58:26 2024
 
 @author: thaihd
 """
+import sqlite3
 from flask import Flask,request,jsonify
+
 app=Flask(__name__)
 app.app_context().push()
 
+conn = sqlite3.connect("jdbc:postgresql://cameraip.cilyqldqbjqk.ap-southeast-1.rds.amazonaws.com:5432/postgres", check_same_thread=False)
+#conn = sqlite3.connect("/Users/thaihd/Database/NewDatabase.db", check_same_thread=False)
+c=conn.cursor()
+tableName="Camera"
+def DropCameraTable():
+    c.execute(f""" DROP TABLE IF EXISTS {tableName}
+                """)
+#Create Database
+c.execute(f""" CREATE TABLE IF NOT EXISTS {tableName}(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                username VACHAR,
+                password VACHAR,
+                ip VACHAR,
+                port VACHAR,
+                status VACHAR
+            )""")
+
 #Create Camera
 class Camera:
+
     def __init__(self,id,username,password,ip,port,status):
         self.id=id
         self.username=username
@@ -38,6 +58,7 @@ class Camera:
             'status': self.status,
         }
 CameraList = []
+
 @app.route("/create-camera",methods=["POST"])
 def CreateCameraRequest():
     data= request.get_json()
@@ -48,6 +69,9 @@ def CreateCameraRequest():
 def CreateNewCamera(id,username,password,ip,port,status):
     newCamera = Camera(id,username,password,ip,port,status)    
     CameraList.append(newCamera)
+    c.execute(f"INSERT INTO {tableName} (username, password, ip, port,status)\
+        VALUES('{username}','{password}','{ip}','{port}','{status}')")
+    conn.commit()
     print("Create New Camera succeed")
     
 @app.route("/update-camera",methods=["PUT"])
@@ -58,6 +82,11 @@ def UpdateCameraRequest():
                     data["port"], data["status"])
     return jsonify(data),201
 def UpdateCamera(id,username,password,ip,port,status):
+    c.execute("UPDATE {tableName} \
+              SET username = '{username}', password = '{password}', \
+                  ip = '{ip}', port = '{port}', \
+                  status = '{status}'\
+              WHERE id='{id}'")
     for itemCamera in CameraList:
         if itemCamera.id == id:
             itemCamera.username=username
@@ -86,12 +115,6 @@ def GetCamera(camera_id):
         if itemCamera.id == id:
             camera = itemCamera.serialize()
             return jsonify(camera),200
-
-
-CreateNewCamera(1, "test", "password", "ip", "port", "status")  
-CreateNewCamera(2, "test", "password", "ip", "port", "status")    
-UpdateCamera(1, "changed", "newPass", "newIp", "newPort", "newStatus")    
-DeleteCamera(2)
 
 
 if(__name__=="__main__"):
