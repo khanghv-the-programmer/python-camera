177f16b381065c5cb9d3f6e6c6716cfba105cff8
TTD262 <duytruong2621998@gmail.com>
2024-01-29 00:19:17 +0700

    update File Test

--- Services/Handler/test.py
+++
@@ -1,7 +0,0 @@
-data = {'Users':[{'full_name':'Test','user_name':'admin','password':'admin'},{'full_name':'Test1','user_name':'admin1','password':'admin1'}]}
-
-for user in data['Users']:
-    full_name = user['full_name']
-    user_name = user['user_name']
-    password = user['password']
-    print('fullname: '+full_name+' username: '+user_name)
